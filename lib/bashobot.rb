1c6d92f245b3ba43a7036f1b0a05daef3c963fcf
Sean Cribbs <seancribbs@gmail.com>
2011-01-17 10:28:47 -0500

    Add authentication so we can set automodes on the bot.

--- lib/bashobot.rb
+++ lib/bashobot.rb
@@ -1,12 +1,15 @@
 require 'cinch'
 require 'bashobot/bugzilla'
 require 'bashobot/wiki'
+require 'yaml'
 
 BashoBot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.net"
     c.channels = %w{#riak}
     c.nick = "bashobot"
+    c.realname = "BashoBot :: https://github.com/basho/bashobot"
+    c.password = YAML.load_file(File.expand_path("../../auth.yml", __FILE__))['password']
     c.plugins.plugins = [Bugzilla, Wiki]
   end
 end
