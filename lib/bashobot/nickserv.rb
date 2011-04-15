32cdc38a2d55a54df10346e55cba8a71ff13e41d
Sean Cribbs <seancribbs@gmail.com>
2011-04-15 16:09:10 -0400

    Make sure bashobot tries to re-register periodically, fork/detach the process on startup.

--- lib/bashobot/nickserv.rb
+++ lib/bashobot/nickserv.rb
@@ -3,17 +3,27 @@ require 'yaml'
 
 class Nickserv
   include Cinch::Plugin
+
   react_on :private
   prefix ''
   suffix ''
-  
+
   match /This nickname is registered/
 
   def password
     @password ||= YAML.load_file(File.expand_path("../../../auth.yml", __FILE__))['password']
   end
-  
+
   def execute(*args)
-    User('NickServ').notice("identify #{password}")
+    User('NickServ').msg("identify #{password}")
+  end
+
+  timer 300, method: :claim_nick
+
+  def claim_nick
+    if bot.nick != 'bashobot'
+      User('NickServ').msg("ghost bashobot #{password}") unless User('bashobot').unknown?
+      bot.nick = 'bashobot'
+    end
   end
 end
